dc06dba009712e4507c31c30896a4f7c421244b9
Saulo Costa <saulotarsobc@gmail.com>
2022-06-14 12:04:09 -0300

    :fre:

--- functions.php
+++ functions.php
@@ -2,35 +2,30 @@
 
 function getTemperature()
 {
-    $output = exec('python3 ./python/main.py getTemperature');
+    $output = shell_exec('python3 ./python/main.py getTemperature');
     return ($output);
 }
 
 function getVoltage1()
 {
-    $output = exec('python3 ./python/main.py getVoltage1');
+    $output = shell_exec('python3 ./python/main.py getVoltage1');
     return ($output);
 }
 
 function getVoltage2()
 {
-    $output = exec('python3 ./python/main.py getVoltage2');
+    $output = shell_exec('python3 ./python/main.py getVoltage2');
     return ($output);
 }
 
 function getVoltage3()
 {
-    $output = exec('python3 ./python/main.py getVoltage3');
+    $output = shell_exec('python3 ./python/main.py getVoltage3');
     return ($output);
 }
 
 function manager()
 {
     $output = shell_exec('ls -ltr --time-style=\'+%s\'');
-    // echo $output;
-    $re = '/^((?!^total\s\d+).*).*\s+\d+\s+(.*)\s(\d+)\s+(\d+)\s+(\d+)\s(.*)$/m';
-    preg_match_all($re, $output, $matches, PREG_SET_ORDER, 0);
-
-    // Print the entire match result
-    echo json_encode($matches);
+    return json_encode($output);
 }
