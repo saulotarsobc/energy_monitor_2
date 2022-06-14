af653c4bce1767a86d4ee26e21e566fbe60a4842
Saulo Costa <saulotarsobc@gmail.com>
2022-06-14 12:29:21 -0300

    :fre:

--- python/main.py
+++ python/main.py
@@ -9,16 +9,22 @@ metodo = sys.argv[1]
 # retornar temperaturas
 if metodo == 'getTemperature':
     final = []
+    
+    
+    
     print(json.dumps(final))
     exit()
+    
 
 if metodo == 'getVoltage1':
     print(json.dumps('py getVoltage1'))
     exit()
+    
 
 if metodo == 'getVoltage2':
     print(json.dumps('py getVoltage2'))
     exit()
+    
 
 if metodo == 'getVoltage3':
     print(json.dumps('py getVoltage3'))
