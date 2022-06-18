d80997e33d75a822ef9d9a7503494163a9bc4525
Saulo Costa <saulotarsobc@gmail.com>
2022-06-18 09:24:59 -0300

    :fre:

--- python/serial.py
+++ python/serial.py
@@ -1,4 +1,5 @@
 import serial
+from time import sleep
 
 # PORTA_SERIAL = "/dev/ttyAMA0"
 PORTA_SERIAL = '/dev/ttyUSB0'
@@ -7,6 +8,7 @@ PORTA_SERIAL = '/dev/ttyUSB0'
 SPEED = 115200
 
 comport = serial(PORTA_SERIAL, SPEED)
+sleep(1)
 VALUE = comport.readline()
 comport.close()
 
