b781533fab036e067ec0fb1332cbee34b56453cd
Saulo Costa <saulotarsobc@gmail.com>
2022-06-18 09:40:01 -0300

    :fre:

--- python/serial.py
+++ python/serial.py
@@ -1,8 +1,8 @@
 import serial
 from time import sleep
 
-# PORTA_SERIAL = "/dev/ttyAMA0"
-PORTA_SERIAL = '/dev/ttyUSB0'
+PORTA_SERIAL = "/dev/ttyAMA0"
+# PORTA_SERIAL = '/dev/ttyUSB0'
 # PORTA_SERIAL = 'COM8'
 
 SPEED = 115200
